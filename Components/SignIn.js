c46bf7f5c0a274b30baddf960d47e51f7ceedb42
muqadasghazal <ghazalmuqadas@gmail.com>
2024-11-15 09:19:32 +0500

    validation added

--- Components/SignIn.js
+++ Components/SignIn.js
@@ -2,64 +2,103 @@ import { Image, KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity, View, 
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Entypo';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+
+
+//backend say aik aur error bhi show krwana ho ga idhr , pasword or email incorrect wala.
+const SignInSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+});
 
 export default function SignIn({ navigation }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [backendError, setBackendError] = useState(''); // To store backend error
     const [passwordVisible, setPasswordVisible] = useState(false);
 
+    const handleSignIn = async (values) => {
+        // Here you can call your backend API for login
+        // For example:
+        try {
+            // await signInApi(values.email, values.password);
+            // If success, navigate to home or other page
+            console.log("Successfully signed in");
+        } catch (error) {
+            // Set backend error if login fails (example error message)
+            setBackendError('Incorrect email or password');
+        }
+    };
+
     return (
-        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-            <SafeAreaView style={styles.innerContainer}>
-                <Image style={styles.imageStyle} source={require('../assets/images/SignUpDesign.png')} />
-                <Text style={styles.text}>Sign In</Text>
+        <Formik
+            initialValues={{ email: '', password: '' }}
+            validationSchema={SignInSchema}
+            onSubmit={handleSignIn}
+        >
+            {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+                    <SafeAreaView style={styles.innerContainer}>
+                        <Image style={styles.imageStyle} source={require('../assets/images/SignUpDesign.png')} />
+                        <Text style={styles.text}>Sign In</Text>
 
-                {/* Email Input */}
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        placeholder="Email"
-                        style={styles.input}
-                        value={email}
-                        onChangeText={setEmail}
-                        keyboardType="email-address"
-                        placeholderTextColor="#999"
-                    />
-                </View>
+                        <View style={styles.formContainer}>
+                            {/* Email Input */}
+                            <View style={styles.inputContainer}>
+                                <TextInput
+                                    placeholder="Email"
+                                    style={styles.input}
+                                    value={values.email}
+                                    onChangeText={handleChange('email')}
+                                    onBlur={handleBlur('email')}
+                                    keyboardType="email-address"
+                                    placeholderTextColor="#999"
+                                />
+                            </View>
+                            {errors.email && touched.email && <Text style={styles.errorText}>{errors.email}</Text>}
 
-                {/* Password Input with Visibility Toggle */}
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        placeholder="Password"
-                        style={styles.input}
-                        value={password}
-                        onChangeText={setPassword}
-                        secureTextEntry={!passwordVisible}
-                        placeholderTextColor="#999"
-                    />
-                    <TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)} style={{ paddingRight: 6 }}>
-                        <Icon name={passwordVisible ? 'eye' : 'eye-with-line'} size={24} color="gray" />
-                    </TouchableOpacity>
-                </View>
+                            {/* Password Input with Visibility Toggle */}
+                            <View style={styles.inputContainer}>
+                                <TextInput
+                                    placeholder="Password"
+                                    style={styles.input}
+                                    value={values.password}
+                                    onChangeText={handleChange('password')}
+                                    onBlur={handleBlur('password')}
+                                    secureTextEntry={!passwordVisible}
+                                    placeholderTextColor="#999"
+                                />
+                                <TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)} style={{ paddingRight: 6 }}>
+                                    <Icon name={passwordVisible ? 'eye' : 'eye-with-line'} size={24} color="gray" />
+                                </TouchableOpacity>
+                            </View>
+                            {errors.password && touched.password && <Text style={styles.errorText}>{errors.password}</Text>}
 
-                {/* Forgot Password Link */}
-                <TouchableOpacity style={styles.forgotPasswordContainer} onPress={() => console.log('should be calling API here')}>
-                    <Text style={styles.forgotPasswordText}>Forgot Your Password?</Text>
-                </TouchableOpacity>
+                            {/* Backend Error Display */}
+                            {backendError ? <Text style={styles.errorText}>{backendError}</Text> : null}
 
-                {/* Sign In Button */}
-                <TouchableOpacity style={styles.signInButton} onPress={() => console.log('should be calling API here')}>
-                    <Text style={styles.signInText}>Sign In</Text>
-                </TouchableOpacity>
+                            {/* Forgot Password Link */}
+                            <TouchableOpacity style={styles.forgotPasswordContainer} onPress={() => console.log('Forgot Password')}>
+                                <Text style={styles.forgotPasswordText}>Forgot Your Password?</Text>
+                            </TouchableOpacity>
 
-                {/* Sign Up Text Row */}
-                <View style={styles.signUpContainer}>
-                    <Text style={styles.signUpText}>Don’t have an account? </Text>
-                    <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-                        <Text style={styles.signUpLink}>Sign Up</Text>
-                    </TouchableOpacity>
-                </View>
-            </SafeAreaView>
-        </KeyboardAvoidingView>
+                            {/* Sign In Button */}
+                            <TouchableOpacity style={styles.signInButton} onPress={handleSubmit}>
+                                <Text style={styles.signInText}>Sign In</Text>
+                            </TouchableOpacity>
+
+                            {/* Sign Up Text Row */}
+                            <View style={styles.signUpContainer}>
+                                <Text style={styles.signUpText}>Don’t have an account? </Text>
+                                <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+                                    <Text style={styles.signUpLink}>Sign Up</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </SafeAreaView>
+                </KeyboardAvoidingView>
+            )}
+        </Formik>
     );
 }
 
@@ -82,11 +121,15 @@ const styles = StyleSheet.create({
         color: '#000000',
         marginVertical: 5,
     },
+    formContainer: {
+        width: '80%',
+        alignItems: 'center',
+    },
     inputContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         marginBottom: 15,
-        width: '80%',
+        width: '100%',
         paddingVertical: 2,
         backgroundColor: '#F1F2F6',
         borderRadius: 10,
@@ -100,17 +143,15 @@ const styles = StyleSheet.create({
     },
     forgotPasswordContainer: {
         alignSelf: 'flex-end',
-        marginRight: '10%', // adjust based on alignment preference
-        marginTop: -10, // slight adjustment to bring it closer to the input field
+
         marginBottom: 20
     },
     forgotPasswordText: {
         color: '#999',
-
     },
     signInButton: {
         backgroundColor: '#22577A',
-        width: '80%',
+        width: '100%',
         borderRadius: 30,
         paddingVertical: 10,
         alignItems: 'center',
@@ -118,7 +159,6 @@ const styles = StyleSheet.create({
     signInText: {
         color: '#FFFFFF',
         fontFamily: 'Poppins-SemiBold',
-        fontSize: 16,
     },
     signUpContainer: {
         flexDirection: 'row',
@@ -133,5 +173,12 @@ const styles = StyleSheet.create({
         color: '#22577A',
         fontWeight: 'bold',
         textDecorationLine: 'underline',
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 12,
+        alignSelf: 'flex-start',
+        marginTop: -10,
+        marginBottom: 10,
     },
 });
--- Components/SignUp.js
+++ Components/SignUp.js
@@ -2,88 +2,142 @@ import { Image, KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity, View, 
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Entypo';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const SignUpSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    firstName: Yup.string().required('First name is required'),
+    lastName: Yup.string().required('Last name is required'),
+    password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Passwords must match')
+        .required('Confirm Password is required'),
+});
 
 export default function SignUp({ navigation }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
     const [passwordVisible, setPasswordVisible] = useState(false);
-    const [confirmPassword, setConfirmPassword] = useState('');
     const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 
-    const renderInputField = (placeholder, value, setValue, isPassword = false, toggleVisibility = null, isConfirmPassword = false) => (
-        <View style={styles.inputContainer}>
-            <TextInput
-                placeholder={placeholder}
-                style={styles.input}
-                value={value}
-                onChangeText={setValue}
-                secureTextEntry={(isPassword && !passwordVisible) || (isConfirmPassword && !confirmPasswordVisible)}
-                keyboardType={placeholder === "Email" ? "email-address" : "default"}
-                placeholderTextColor="#999"
-            />
-            {(isPassword || isConfirmPassword) && (
-                <TouchableOpacity onPress={toggleVisibility} style={{ paddingRight: 6 }}>
-                    <Icon
-                        name={(isPassword ? passwordVisible : confirmPasswordVisible) ? 'eye' : 'eye-with-line'}
-                        size={24}
-                        color="gray"
-                    />
-                </TouchableOpacity>
-            )}
+    const renderInputField = (placeholder, value, handleChange, handleBlur, error, touched, isPassword = false, toggleVisibility = null, isConfirmPassword = false) => (
+        <View style={styles.inputWrapper}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder={placeholder}
+                    style={styles.input}
+                    value={value}
+                    onChangeText={handleChange}
+                    onBlur={handleBlur}
+                    secureTextEntry={(isPassword && !passwordVisible) || (isConfirmPassword && !confirmPasswordVisible)}
+                    keyboardType={placeholder === "Email" ? "email-address" : "default"}
+                    placeholderTextColor="#999"
+                />
+                {(isPassword || isConfirmPassword) && (
+                    <TouchableOpacity onPress={toggleVisibility} style={{ paddingRight: 6 }}>
+                        <Icon
+                            name={(isPassword ? passwordVisible : confirmPasswordVisible) ? 'eye' : 'eye-with-line'}
+                            size={24}
+                            color="gray"
+                        />
+                    </TouchableOpacity>
+                )}
+            </View>
+            {error && touched ? <Text style={styles.errorText}>{error}</Text> : null}
         </View>
     );
 
     return (
-        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-            <SafeAreaView style={styles.innerContainer}>
-                <Image style={styles.imageStyle} source={require('../assets/images/SignUpDesign.png')} />
-                <Text style={styles.text}>Sign Up</Text>
+        <Formik
+            initialValues={{
+                email: '',
+                firstName: '',
+                lastName: '',
+                password: '',
+                confirmPassword: '',
+            }}
+            validationSchema={SignUpSchema}
+            onSubmit={(values) => {
+                console.log(values);
+                navigation.navigate('SignIn');
+            }}
+        >
+            {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+                    <SafeAreaView style={styles.innerContainer}>
+                        <Image style={styles.imageStyle} source={require('../assets/images/SignUpDesign.png')} />
+                        <Text style={styles.text}>Sign Up</Text>
 
-                <View style={styles.formContainer}>
-                    {/* Email Input */}
-                    {renderInputField("Email", email, setEmail)}
+                        <View style={styles.formContainer}>
+                            {renderInputField(
+                                "Email",
+                                values.email,
+                                handleChange('email'),
+                                handleBlur('email'),
+                                errors.email,
+                                touched.email
+                            )}
 
-                    {/* First and Last Name Input */}
-                    <View style={styles.rowContainer}>
-                        <TextInput
-                            placeholder="First Name"
-                            style={[styles.input, styles.halfWidthInput, { marginRight: 5 }]}
-                            value={firstName}
-                            onChangeText={setFirstName}
-                            placeholderTextColor="#999"
-                        />
-                        <TextInput
-                            placeholder="Last Name"
-                            style={[styles.input, styles.halfWidthInput]}
-                            value={lastName}
-                            onChangeText={setLastName}
-                            placeholderTextColor="#999"
-                        />
-                    </View>
+                            <View style={styles.rowContainer}>
+                                <View style={{ flex: 1, marginRight: 5 }}>
+                                    {renderInputField(
+                                        "First Name",
+                                        values.firstName,
+                                        handleChange('firstName'),
+                                        handleBlur('firstName'),
+                                        errors.firstName,
+                                        touched.firstName
+                                    )}
+                                </View>
+                                <View style={{ flex: 1 }}>
+                                    {renderInputField(
+                                        "Last Name",
+                                        values.lastName,
+                                        handleChange('lastName'),
+                                        handleBlur('lastName'),
+                                        errors.lastName,
+                                        touched.lastName
+                                    )}
+                                </View>
+                            </View>
 
-                    {/* Password Input */}
-                    {renderInputField("Password", password, setPassword, true, () => setPasswordVisible(!passwordVisible))}
+                            {renderInputField(
+                                "Password",
+                                values.password,
+                                handleChange('password'),
+                                handleBlur('password'),
+                                errors.password,
+                                touched.password,
+                                true,
+                                () => setPasswordVisible(!passwordVisible)
+                            )}
 
-                    {/* Confirm Password Input */}
-                    {renderInputField("Confirm Password", confirmPassword, setConfirmPassword, false, () => setConfirmPasswordVisible(!confirmPasswordVisible), true)}
+                            {renderInputField(
+                                "Confirm Password",
+                                values.confirmPassword,
+                                handleChange('confirmPassword'),
+                                handleBlur('confirmPassword'),
+                                errors.confirmPassword,
+                                touched.confirmPassword,
+                                false,
+                                () => setConfirmPasswordVisible(!confirmPasswordVisible),
+                                true
+                            )}
 
-                    {/* Next button */}
-                    <TouchableOpacity style={styles.nextButton} onPress={() => navigation.navigate('SignIn')}>
-                        <Text style={styles.nextText}>Next</Text>
-                    </TouchableOpacity>
+                            <TouchableOpacity style={styles.nextButton} onPress={handleSubmit}>
+                                <Text style={styles.nextText}>Next</Text>
+                            </TouchableOpacity>
 
-                    {/* Already have an account? */}
-                    <View style={styles.signInContainer}>
-                        <Text style={styles.signInText}>Already have an account? </Text>
-                        <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
-                            <Text style={styles.signInLink}>Sign In</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </SafeAreaView>
-        </KeyboardAvoidingView>
+                            <View style={styles.signInContainer}>
+                                <Text style={styles.signInText}>Already have an account? </Text>
+                                <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
+                                    <Text style={styles.signInLink}>Sign In</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </SafeAreaView>
+                </KeyboardAvoidingView>
+            )}
+        </Formik>
     );
 }
 
@@ -110,27 +164,23 @@ const styles = StyleSheet.create({
         width: '80%',
         alignItems: 'center',
     },
+    inputWrapper: {
+        width: '100%',
+        marginBottom: 5,
+    },
     inputContainer: {
         flexDirection: 'row',
         alignItems: 'center',
-        marginBottom: 15,
-        width: '100%',
-        paddingVertical: 2,
         backgroundColor: '#F1F2F6',
         borderRadius: 10,
         paddingRight: 2,
+        paddingVertical: 2,
     },
     rowContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         width: '100%',
-        marginBottom: 15,
-        paddingVertical: 2,
-    },
-    halfWidthInput: {
-        width: '48%',
-        backgroundColor: '#F1F2F6',
-        borderRadius: 10,
+        marginBottom: 5,
     },
     input: {
         flex: 1,
@@ -142,27 +192,32 @@ const styles = StyleSheet.create({
         backgroundColor: '#22577A',
         width: "100%",
         borderRadius: 30,
-        paddingVertical: 3
+        paddingVertical: 10,
+        marginTop: 10,
     },
     nextText: {
-
         fontFamily: 'Poppins-SemiBold',
         textAlign: 'center',
         color: '#FFFFFF',
-        marginVertical: 5,
     },
     signInContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
+        marginTop: 20,
     },
     signInText: {
         color: '#333',
-        marginVertical: 20
     },
     signInLink: {
         color: '#22577A',
         fontWeight: 'bold',
-        textDecorationLine: 'underline'
+        textDecorationLine: 'underline',
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 12,
+        marginLeft: 15,
+        marginTop: 2,
     },
 });
