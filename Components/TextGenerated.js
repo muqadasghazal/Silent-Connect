ef835ae936c3bf22122b95c3523f15a12b40ffa7
muqadasghazal <141500829+muqadasghazal@users.noreply.github.com>
2025-04-23 11:27:16 +0500

    Added loading modal and styled text generated

--- Components/TextGenerated.js
+++ Components/TextGenerated.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const TextGenerated = ({ route }) => {
+
+const TextGenerated = ({ route, navigation }) => {
     const translatedText = route.params.translatedText; // Retrieve the response text
+    const unrecognizedCount = route.params.unrecognizedGestures;
+    const processingTime = route.params.processingTime
 
     return (
         <View style={styles.container}>
-            <Text style={styles.resultText}>Generated Text:</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <TouchableOpacity style={{ marginRight: 9 }} onPress={() => navigation.navigate('Dashboard')}>
+                    <Icon name='arrow-back' size={24} color="#22577A" />
+                </TouchableOpacity>
+                <Text style={styles.text}>Generated Text</Text>
+            </View>
             <Text style={styles.translatedText}>{translatedText}</Text>
+            <Text style={styles.remainingText}>Unrecognized Gestures: {unrecognizedCount}</Text>
+            <Text style={styles.remainingText}>Processing Time : {processingTime}</Text>
         </View>
     );
 };
@@ -15,20 +26,28 @@ const TextGenerated = ({ route }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
+        paddingTop: 50,
+        paddingHorizontal: 10,
+        backgroundColor: "#FFFFFF"
+
     },
-    resultText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 10,
+    text: {
+        fontFamily: 'Poppins-SemiBold',
+        fontSize: 24,
+        color: '#22577A'
     },
+
     translatedText: {
         fontSize: 18,
+        fontFamily: 'Poppins-SemiBold',
         padding: 10,
         color: 'black'
     },
+    remainingText: {
+        color: 'green',
+        fontFamily: 'Poppins-SemiBold',
+        fontSize: 18,
+    }
 });
 
 export default TextGenerated;
